7d12043cc2a1e6d76d89532d9ce29939f41c9f5c
Felipe Lemos <139652554+flplemos@users.noreply.github.com>
2024-03-13 16:55:35 -0300

    newsletter-sign-up-with-success-message-main
    Project upload.

---
+++ newsletter-sign-up-with-success-message-main/src/script.js
@@ -0,0 +1,44 @@
+function validarEmail() {
+    var email = document.getElementById('email');
+    var error = document.getElementById('error-mail');
+
+    if (!email.checkValidity()) {
+        error.innerHTML = "Valid email required!";
+        email.style.background = 'rgba(220, 20, 60, 0.301)'
+    }
+}
+
+function redefinirMsg(){
+    var error = document.getElementById('error-mail');
+
+    if (error.innerHTML = "Valid email required!"){
+        
+        error.innerHTML = "";
+        email.style.background = "white";
+    }
+}
+
+function Enviar(){
+    var email = document.getElementById('email');
+    var popup = document.querySelector('.popup');
+    var proxpage = document.querySelector('.desativado');
+    var emailDisplay = document.getElementById('emailDisplay'); // Novo elemento
+
+    if (email.checkValidity()){
+        popup.classList.toggle("desativado");
+        proxpage.classList.toggle("ativado");
+        
+        // Exibe o email digitado no novo elemento
+        emailDisplay.innerHTML = "A confirmation email has been sent to <strong>" + email.value + "</strong>. Please open it and click the button inside to confirm your subscription.";
+    } else {
+        window.alert('Valid email required!')
+    }
+}
+
+function Voltar() {
+    var popup = document.querySelector('.popup');
+    var proxpage = document.getElementById('newcard');
+
+    popup.classList.toggle("desativado");
+    proxpage.classList.toggle("ativado");
+}
